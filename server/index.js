2a19c17445e379db8d746d3521371a2355c1d1ae
vikalpis <149229170+vikalpis@users.noreply.github.com>
2025-05-19 21:49:38 +0530

    restrict CORS to specific origin for enhanced security

--- server/index.js
+++ server/index.js
@@ -14,7 +14,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors(
+  ["https://track-my-mood-lac.vercel.app/"]
+));
 app.use(express.json());
 
 // Connect to MongoDB
